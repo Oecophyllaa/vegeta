169e9b8b99ca70fc2a3e0ab22ba51c8478dc2a23
Oecophyllaa <rangga.godzila@gmail.com>
2024-08-03 23:44:08 +0700

    init & setup auth form validation

--- src/app/auth/signup/sign-up-form.tsx
+++ src/app/auth/signup/sign-up-form.tsx
@@ -5,50 +5,70 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { hover } from "@/lib/hover";
 import { cn } from "@/lib/utils";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+type UserAuthForm = {
+  name: string;
+  email: string;
+  password: string;
+  confirm_password: string | undefined;
+};
+
+const schema = yup
+  .object({
+    name: yup.string().required(),
+    email: yup.string().email().required(),
+    password: yup.string().min(6).required(),
+    confirm_password: yup.string().oneOf([yup.ref("password")], "Password must match"),
+  })
+  .required();
 
 function SignUpForm() {
   const [showPassword, setShowPassword] = useState(false);
-  const [showConfirmationPassword, setShowConfirmationPassword] =
-    useState(false);
+  const [showConfirmationPassword, setShowConfirmationPassword] = useState(false);
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<UserAuthForm>({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data: UserAuthForm) => {
+    console.log("🚀 ~ onSubmit ~ data:", data);
+  };
 
   return (
-    <form className="flex flex-col w-[100%] gap-4 items-center">
-      <div className="w-[100%] text-3xl font-semibold tracking-widest mb-2 text-center">
-        Buat akun baru
-      </div>
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-[100%]">
+      <div className="w-[100%] text-3xl font-semibold tracking-widest mb-2 text-center">Buat akun baru</div>
+      <Input className="w-[100%] p-4 rounded-sm mt-4" type="text" placeholder="Nama Lengkap" {...register("name")} error={errors.name?.message} />
+
+      <Input className="w-[100%] p-4 rounded-sm mt-4" type="text" placeholder="Email" {...register("email")} error={errors.email?.message} />
+
       <Input
-        className="w-[100%] p-4 rounded-sm"
-        type="text"
-        placeholder="Nama Lengkap"
-      />
-      <Input
-        className="w-[100%] p-4 rounded-sm"
-        type="text"
-        placeholder="Email"
+        className="w-[100%] p-4 rounded-sm mt-4"
+        type={showPassword ? "text" : "password"}
+        placeholder="Kata Sandi"
+        suffix="Eye"
+        onPressSuffix={() => setShowPassword(!showPassword)}
+        {...register("password")}
+        error={errors.password?.message}
       />
 
-      <div className="w-[100%] relative">
-        <Input
-          className="w-[100%] p-4 rounded-sm"
-          type={showPassword ? "text" : "password"}
-          placeholder="Kata Sandi"
-          suffix="Eye"
-          onPressSuffix={() => setShowPassword(!showPassword)}
-        />
-      </div>
-      <div className="w-[100%] relative">
-        <Input
-          className="w-[100%] p-4 rounded-sm"
-          type={showConfirmationPassword ? "text" : "password"}
-          placeholder="Konfirmasi Kata Sandi"
-          suffix="Eye"
-          onPressSuffix={() =>
-            setShowConfirmationPassword(!showConfirmationPassword)
-          }
-        />
-      </div>
+      <Input
+        className="w-[100%] p-4 rounded-sm mt-4"
+        type={showConfirmationPassword ? "text" : "password"}
+        placeholder="Konfirmasi Kata Sandi"
+        suffix="Eye"
+        onPressSuffix={() => setShowConfirmationPassword(!showConfirmationPassword)}
+        {...register("confirm_password")}
+        error={errors.confirm_password?.message}
+      />
 
-      <Button className={cn("w-[320px] bg-leaf mt-6", hover.shadow)}>
+      <Button type="submit" className={cn("w-[320px] bg-leaf mt-6 mx-auto", hover.shadow)}>
         Buat Akun
       </Button>
     </form>
